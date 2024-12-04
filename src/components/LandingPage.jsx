0d4f06e16f738f611cdee1cb3bec47dbe13c3697
Penguin2 <vanessleonard.capuras@cit.edu>
2024-12-04 16:36:08 +0800

    yess

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -491,7 +491,7 @@ return (
             <div className="footer-section">
               <h3>Features</h3>
               <ul>
-                <li><a href="/admin/signup">Admin</a></li>
+                <li><Link to="/admin/signup" className="admin-link">Admin</Link></li>
                 <li><a>Engagement Tools</a></li>
                 <li><a>Community Building</a></li>
               </ul>
